1219f34adcea9cc17f4c05c40c80f44b0aa6ca51
Adonis <3063545118@qq.com>
2023-11-16 15:39:14 +0800

    ex 2 commit by 21301141 ydh in 11/16

---
+++ os/target/release/deps/os-fd19cf0985069362.d
@@ -0,0 +1,9 @@
+/mnt/os/target/release/deps/os-fd19cf0985069362: src/main.rs src/console.rs src/lang_items.rs src/sbi.rs src/entry.asm
+
+/mnt/os/target/release/deps/os-fd19cf0985069362.d: src/main.rs src/console.rs src/lang_items.rs src/sbi.rs src/entry.asm
+
+src/main.rs:
+src/console.rs:
+src/lang_items.rs:
+src/sbi.rs:
+src/entry.asm:
