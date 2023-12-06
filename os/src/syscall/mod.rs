a663e51dc6129435a0ca7f4a6ddef88efb378674
Adonis <3063545118@qq.com>
2023-12-06 11:36:01 +0800

    ex4 git by yangdonghai 21301141 in 12/6

--- os/src/syscall/mod.rs
+++ os/src/syscall/mod.rs
@@ -1,5 +1,6 @@
 const SYSCALL_WRITE: usize = 64;
 const SYSCALL_EXIT: usize = 93;
+const SYSCALL_YIELD: usize = 124;
 
 mod fs;
 mod process;
@@ -11,24 +12,7 @@ pub fn syscall(syscall_id: usize, args: [usize; 3]) -> isize {
     match syscall_id {
         SYSCALL_WRITE => sys_write(args[0], args[1] as *const u8, args[2]),
         SYSCALL_EXIT => sys_exit(args[0] as i32),
+        SYSCALL_YIELD => sys_yield(),
         _ => panic!("Unsupported syscall_id: {}", syscall_id),
-    }
-}
-
-// os/src/syscall/fs.rs
-
-const FD_STDOUT: usize = 1;
-
-pub fn sys_write(fd: usize, buf: *const u8, len: usize) -> isize {
-    match fd {
-        FD_STDOUT => {
-            let slice = unsafe { core::slice::from_raw_parts(buf, len) };
-            let str = core::str::from_utf8(slice).unwrap();
-            print!("{}", str);
-            len as isize
-        },
-        _ => {
-            panic!("Unsupported fd in sys_write!");
-        }
     }
 }
--- os/target/release/deps/os-fd19cf0985069362.d
+++
@@ -1,9 +0,0 @@
-/mnt/os/target/release/deps/os-fd19cf0985069362: src/main.rs src/console.rs src/lang_items.rs src/sbi.rs src/entry.asm
-
-/mnt/os/target/release/deps/os-fd19cf0985069362.d: src/main.rs src/console.rs src/lang_items.rs src/sbi.rs src/entry.asm
-
-src/main.rs:
-src/console.rs:
-src/lang_items.rs:
-src/sbi.rs:
-src/entry.asm:
